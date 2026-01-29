ba73129782e7afd5283d004bfb797e93ad3f7051
HR <kddo3339@gmail.com>
2026-01-29 16:36:24 +0900

    domain email send successfully

--- server/ims-server/src/main/java/com/example/ims/features/invitation/services/InvitationService.java
+++ server/ims-server/src/main/java/com/example/ims/features/invitation/services/InvitationService.java
@@ -19,31 +19,33 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class InvitationService {
 
-    private static final Duration INVITE_TTL = Duration.ofMinutes(5);
+    private static final Duration INVITE_TTL 
+        = Duration.ofMinutes(5);
 
     private final InvitationTokenStore tokenStore;
     private final ResendProperties props;
 
     public void invite(EmailRequest request) throws ResendException {
-        List<String> emails = request.getEmails();
-
         Resend resend = new Resend(props.getApiKey());
 
-        for (String email: emails) {
-            String token = UUID.randomUUID().toString();
-            tokenStore.save(token, email, INVITE_TTL);
-            sendInvite(resend, email, token);
-        }
+        List<CreateEmailOptions> batch = request.getEmails()
+            .stream()
+            .map(email -> {
+                String token = UUID.randomUUID().toString();
+                tokenStore.save(token, email, INVITE_TTL);
+                return getEmailOptions(resend, email, token);
+            }).toList();
+
+        resend.batch().send(batch);
     }
 
-    public void sendInvite(Resend resend, String email, String token) throws ResendException {
-        CreateEmailOptions param = CreateEmailOptions.builder()
+    public CreateEmailOptions getEmailOptions(
+            Resend resend, String email, String token) {
+        return CreateEmailOptions.builder()
             .from(props.getFromEmail())
             .to(email)
             .subject("Get the code!")
             .html(token)
             .build();
-
-        resend.emails().send(param);
     }
 }

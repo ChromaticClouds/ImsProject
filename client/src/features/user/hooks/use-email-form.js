fbbadce228842a9bab7b62131267a14d92d84bc5
ChromaticClouds <kddo3339@gmail.com>
2026-02-14 01:39:00 +0900

    auth bug fixed

--- client/src/features/user/hooks/use-email-form.js
+++ client/src/features/user/hooks/use-email-form.js
@@ -1,5 +1,6 @@
 // @ts-check
 
+import { toast } from 'sonner';
 import { sendEmail } from '../api/index.js';
 import { emailFormSchema } from '../schemas/email-form-schema.js';
 
@@ -7,6 +8,8 @@ import { emailFormSchema } from '../schemas/email-form-schema.js';
  * Hooks
  */
 import { useForm } from '@tanstack/react-form';
+import { HTTPError } from 'ky';
+import { ERROR } from '@/services/error.js';
 
 const defaultValues = { email: '' };
 
@@ -14,12 +17,27 @@ export const useEmailForm = () => {
   const form = useForm({
     defaultValues,
     validators: {
-      onChange: emailFormSchema
+      onChange: emailFormSchema,
     },
     onSubmit: async ({ value }) => {
-      const response = await sendEmail(value);
-      console.log(response);
-    }
+      try {
+        const response = await sendEmail(value);
+        if (!response.success) return;
+        toast.success(response.message);
+        form.reset();
+      } catch (err) {
+        if (err instanceof HTTPError) {
+          const errResponse = await err.response.json().catch(() => null);
+          return toast.success(
+            typeof errResponse?.message === 'string'
+              ? errResponse?.message
+              : ERROR.UNEXPECTED_ERROR,
+          );
+        }
+
+        toast.error(ERROR.SERVER_ERROR);
+      }
+    },
   });
 
   return form;

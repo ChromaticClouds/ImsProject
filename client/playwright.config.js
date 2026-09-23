a2113f26efb1c337c189f444b90cbec9bcbb5afb
ChromaticClouds <kddo3339@gmail.com>
2026-09-23 20:06:53 +0900

    test: use bundled playwright test runner

--- client/playwright.config.js
+++ client/playwright.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig } from 'playwright/test';
 
 const baseURL = process.env.E2E_BASE_URL ?? 'https://ims-project-pi.vercel.app';
 

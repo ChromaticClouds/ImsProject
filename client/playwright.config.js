ef9b0c755fc9cdae2066bb8cd5c083741687c5b8
ChromaticClouds <kddo3339@gmail.com>
2026-09-23 20:16:26 +0900

    test: limit production e2e retries and workers

--- client/playwright.config.js
+++ client/playwright.config.js
@@ -10,8 +10,8 @@ export default defineConfig({
   },
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 1 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  retries: 0,
+  workers: process.env.CI ? 3 : undefined,
   reporter: [
     ['list'],
     ['html', { outputFolder: 'playwright-report', open: 'never' }],
